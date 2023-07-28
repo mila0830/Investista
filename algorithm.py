0eaca6d13d360cef11b60f48930f39c2f75c22f6
Mila Rushkin <mila.rushkin@mail.mcgill.ca>
2023-07-28 19:18:53 -0400

    Updated all files with making the scraping of symbols and prices accurate but still off in length

--- algorithm.py
+++ algorithm.py
@@ -1,7 +1,8 @@
 import finnhub
 import pandas as pd
 import yfinance as yf
-
+from companies import scrape
+from companies import scrape_p
 
 finnhub_client = finnhub.Client(api_key="cj1em1hr01qhv0uhof90cj1em1hr01qhv0uhof9g")
 
@@ -15,5 +16,18 @@ print(all_data["metric"]["epsTTM"])
 print(price)
 
 msft=yf.Ticker("APPL")
-print(msft.cashflow)
-print(msft.info['currentPrice'])
+#print(msft.cashflow)
+#print(msft.info['currentPrice'])
+
+# Define the URL of the web page that contains the list of NASDAQ 100 companies
+url = "https://markets.businessinsider.com/index/components/nasdaq_100"
+url2="https://markets.businessinsider.com/index/components/nasdaq_100?p=2"
+
+first_half_symbols=scrape("https://markets.businessinsider.com/index/components/nasdaq_100")
+second_half_symbols=scrape("https://markets.businessinsider.com/index/components/nasdaq_100?p=2")
+
+first_half_prices=scrape_p("https://markets.businessinsider.com/index/components/nasdaq_100")
+second_half_prices=scrape_p("https://markets.businessinsider.com/index/components/nasdaq_100?p=2")
+
+print(len(first_half_prices))
+print(len(second_half_prices))
--- companies.py
+++ companies.py
@@ -9,6 +9,7 @@ url2="https://markets.businessinsider.com/index/components/nasdaq_100?p=2"
 
 # Create an empty list to store the company symbols
 symbols = []
+
 def scrape(url):
     # Make a GET request to the URL and store the response object
     response = requests.get(url)
@@ -22,7 +23,7 @@ def scrape(url):
 
         # Find the table element that has the class "table instruments"
         table = soup.find("table", class_="table table__layout--fixed")
-        print(table)
+       
 
         # Find all the table rows that have the class "table__row"
         rows = table.findChildren("tr")
@@ -38,6 +39,7 @@ def scrape(url):
             cell = row.find("td", class_="table__td table__td--big")
             
 
+
             # Check if the cell exists
             if cell:
                 # Get the text content of the cell and strip any whitespace
@@ -52,7 +54,60 @@ def scrape(url):
         # Print an error message if the request failed
         print("Error: sight doesn't work for scraping")
 
-scrape(url)
-scrape(url2)
 
-print(symbols)
+
+def scrape_p(url):
+    #create an empty list to store the company price
+    prices = [] 
+    # Make a GET request to the URL and store the response object
+    response = requests.get(url)
+    # Check if the request was successful
+    if response.status_code == 200:
+
+        # Create a BeautifulSoup object from the response text
+        soup = BeautifulSoup(response.text, "html.parser")
+        
+        
+
+        # Find the table element that has the class "table instruments"
+        table = soup.find("table", class_="table table__layout--fixed")
+       
+
+        # Find all the table rows that have the class "table__row"
+        rows = table.findChildren("tr")
+        print(len(rows))
+        
+
+
+        # Create an empty list to store the company symbols
+        #symbols = []
+
+        # Loop through each row and extract the company symbol
+        for row in rows:
+            
+            cell_p= row.find_all("td")
+            
+
+            if cell_p:
+                price1=cell_p[1]
+                print(price1)
+                price = price1.text.strip()
+                one_price=''
+                for element in price:
+                    if element != "\n":
+                        one_price +=element
+                    else:
+                        break
+                    
+
+                prices.append(one_price)
+
+        # Print the list of symbols
+        
+        return prices
+    else:
+        # Print an error message if the request failed
+        print("Error: sight doesn't work for scraping")
+
+
+
